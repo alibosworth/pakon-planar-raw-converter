dcf0a9b3476ae6c90eafb9719a1a1a0eccbaaa99
Ali Bosworth <ali@metalabdesign.com>
2016-08-13 20:52:17 -0400

    wip

--- index.js
+++ index.js
@@ -1,144 +1,140 @@
 #!/usr/bin/env node
-
 var fs = require( 'fs' );
 var path = require( 'path' );
 var process = require( "process" );
 var glob = require('glob-fs')({ gitignore: true });
-var execSync = require('child_process').execSync;
-var promiseExec = require('child-process-promise').exec
+var promiseExec = require('child-process-promise').exec;
+var Promise = require("bluebird");
+var checkDependencies = require('./lib/check-dependencies');
 
-var currentDir = process.cwd();
+var OUTPUT_DIR = "out";
 
-var commandExists = require('command-exists');
-
-var BYTE_SIZE_TO_DIMENSIONS = {
+var BYTE_SIZE_TO_DIMENSIONS = { // A map of file size to the size value base to Convert's "size" parameter
   "36000000": "3000x2000",     // "Base 16"
   "36000016": "3000x2000+16",  // "Base 16" exported with header
   "20250000": "2250x1500",     // "Base 8"
   "20250016": "2250x1500+16",  // "Base 8" exported with header
   "9000000" : "1500x1000",      // "Base 4"
   "9000016" : "1500x1000+16"   // "Base 4" exported with header
-}
+};
 
-var data = {};
-
-checkDependencies();
-
-var rawFiles = glob.readdirSync('*.raw', {});
-
-
-if (!rawFiles.length) {
-  exit("No raw files found in the current directory");
-} else {
-  console.log(`Found ${rawFiles.length} raw files in current directory...`);
-}
-
-rawFiles.forEach(function(rawFile){
-  var filePath = currentDir + "/" + rawFile;
-  var sizeInBytes = fs.statSync(filePath).size;
-  var dimensionsForConvert = BYTE_SIZE_TO_DIMENSIONS[sizeInBytes.toString()];
-
-  if (!dimensionsForConvert) {
-    console.error(`${rawFile} is the wrong size - please export via TLXClientDemo in "Planar" mode at "Original height and width"`);
-  } else {
-    data[rawFile] = {
-      size: dimensionsForConvert
-    }
-  }
+checkForDependencies().then(function(){
+  var rawFiles = scanDirectoryForFiles();
+  var usableRawFilesWithSizeData = checkRawFileSizes(rawFiles);
+  convertRawFilesToTif(usableRawFilesWithSizeData).then(function(tifs){
+    process.stdout.write("\n");
+    console.log("Converted raw files to tifs, inverting and balancing with negfix8...");
+    adjustTifsWithNegfix8(tifs).then(function(files){
+      process.stdout.write("\n");
+      console.log(`Done. ${files.length} ${files.length === 1 ? "file" : "files"} exported to the '${OUTPUT_DIR}' subdirectory`);
+    });
+  });
 });
 
-if (Object.keys(data).length === 0) {
-  exit("Sorry, no .raw files in the current directory look right")
+function scanDirectoryForFiles () {
+  var rawFiles = glob.readdirSync('*.raw', {});
+
+  if (!rawFiles.length) {
+    exitWithError("No raw files found in the current directory");
+  } else {
+    console.log(`Found ${rawFiles.length} raw files in current directory...`);
+    return rawFiles;
+  }
 }
 
-console.log (`${Object.keys(data).length} raw files are the correct size, converting them...`)
+function checkRawFileSizes(rawFiles){
+  var currentDir = process.cwd();
+  var data = {};
+  var badFiles = [];
+  rawFiles.forEach(function(rawFile){
+    var filePath = currentDir + "/" + rawFile;
+    var sizeInBytes = fs.statSync(filePath).size;
+    var dimensionsForConvert = BYTE_SIZE_TO_DIMENSIONS[sizeInBytes.toString()];
 
-convertRawFilesToTif(data).then(function(tifs){
-  invertAndBalanceTifs(tifs);
-
-})
-
- // convert -size 2250x1500 -depth 16  rgb:1.raw tif:out.tif
-// console.log("data",data);
-
-// fs.readdir( __dirname, function( err, files ) {
-//   if( err ) {
-//       console.error( "Could not list the directory.", err );
-//       process.exit( 1 );
-//   }
-
-//   files.forEach( function( file, index ) {
-//     console.log(file)
-//   });
-// });
-
-function convertRawFilesToTif (data) {
-  var conversionPromises = []
-  for (var item in data) {
-     // convert -size 2250x1500 -depth 16 rgb:1.raw tif:out.tif
-     var promise = convertRawToTif(item, data[item].size);
-     conversionPromises.push(promise);
-     promise.catch(function(error) {
-      exit("Error converting a file from a raw to a tiff", item);
-     })
-     // console.log(data[item].size);
-  }
-
-  return Promise.all(conversionPromises).then(values => {
-    console.log(`All ${values.length} raw images converted to tif, adjusting with negfix8`)
-    return values;
+    if (!dimensionsForConvert) {
+      badFiles.push(rawFile);
+      console.error(`${rawFile} is the wrong size - please export via TLXClientDemo in "Planar" mode at "Original height and width"`);
+    } else {
+      data[rawFile] = {
+        size: dimensionsForConvert
+      };
+    }
   });
 
+  var validFileCount = Object.keys(data).length;
+  if (validFileCount === 0) {
+    exitWithError("Sorry, no .raw files in the current directory are the correct size.");
+  } else if (validFileCount === rawFiles.length) {
+    console.log(`All ${validFileCount} files in the current directory are a correct size...`);
+  } else {
+    console.log(`${validFileCount} files will be converted but ${rawFiles.length-validFileCount} (${badFiles.join(",")}) ${badFiles.length === 1 ? "is" : "are"} the wrong size...`);
+  }
+
+  return data;
+}
+
+function convertRawFilesToTif (data) {
+  process.stdout.write("CONVERTING: ");
+  var conversionPromises = [];
+  for (var item in data) {
+     var promise = convertRawToTif(item, data[item].size);
+     conversionPromises.push(promise);
+     promise.then(function() {
+       process.stdout.write(" ▢ ");
+     }).catch(function(error) {
+       exitWithError("Error converting a file from a raw to a tiff", item);
+     });
+  }
+  return Promise.all(conversionPromises);
 }
 
 function convertRawToTif (name, sizeParameter) {
   var baseName = path.basename(name, ".raw");
-  // console.log("basename", baseName);
-  var cmd = `convert -size ${sizeParameter} -depth 16 -interlace plane rgb:${name} -gamma 2.2 tif:${baseName}.tif`
+  var cmd = `convert -size ${sizeParameter} -depth 16 -interlace plane rgb:${name} -gamma 2.2 tif:${baseName}.tif`;
   return promiseExec(cmd).then(function(){
     return `${baseName}.tif`;
-  })
-    // command output is in stdout
-    // console.log(error, stdout, stderr);
-  // });
-  // console.log(cmd);
+  });
 }
 
-// console.log("convertedFiles outer", convertedFiles);
+function adjustTifsWithNegfix8(tifs) {
+  var promises = [];
+  process.stdout.write("ADJUSTING: ");
 
-function invertAndBalanceTifs(tifs) {
-  var dir = "out"
-  if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+  if (!fs.existsSync(OUTPUT_DIR)){
+      fs.mkdirSync(OUTPUT_DIR);
   }
-  // console.log("invertAndBalanceTifs" ,tifs);
   tifs.forEach(function(tif){
-  var cmd = `negfix8 -cs ${tif} out/${tif}`
-    console.log(cmd);
-    return promiseExec(cmd).then(function(){
+    var cmd = `negfix8 -cs ${tif} ${OUTPUT_DIR}/${tif}`;
+    var promise = promiseExec(cmd).then(function(){
+      process.stdout.write(" ▢ ");
       return tif;
     }).catch(function(error){
-      console.log(`Error converting ${tif} to out/${tif}`);
+      console.log(`Error converting ${tif} to ${OUTPUT_DIR}/${tif}`);
     });
+    promises.push(promise);
   });
+  return Promise.all(promises);
 }
 
-function checkDependencies () {
-  console.log ("checkDependencies");
-  commandExists('negfix8', function(err, commandExists) {
+function checkForDependencies() {
+  var promises = [];
 
-    if(commandExists) {
-        console.log("y")
+  promises.push(checkDependencies("convert").then(function(success, error){
+    if (!success) {
+      exitWithError("'convert' from ImageMagick doesn't seem to exist, please install it");
     }
-    if (err) {
-      console.log("err",err);
+  }));
+
+  promises.push(checkDependencies("negfix8").then(function(success, error){
+    if (!success) {
+      exitWithError("'negfix8'doesn't seem to exist, please install it");
     }
+  }));
 
-  });
-
+  return Promise.all(promises);
 }
-function exit (message) {
-  console.error(message);
+
+function exitWithError (message) {
+  console.error("ERROR: "+ message);
   process.exit(1);
 }
-
