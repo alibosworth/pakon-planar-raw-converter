8b0a049fe9f07e0e13c165f266953d5899b3f20f
Ali Bosworth <ali@alibosworth.com>
2017-12-28 11:25:06 -0500

    Site updated to 6c5b412

---
+++ javascripts/script.js
@@ -0,0 +1,1 @@
+(function(){$.featherlight.autoBind=!1,$(document).ready(function(){return $(".container.comparison").length&&$(".comparison-table").featherlightGallery({targetAttr:"href",filter:"a",galleryFadeIn:0,galleryFadeOut:0,openSpeed:0,closeSpeed:0}),$("a[href^='http://'], a[href^='https://']").attr("target","_blank")})}).call(this);
